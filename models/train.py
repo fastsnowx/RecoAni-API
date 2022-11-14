2ccc219fdcfb92cad72c16271e289850721ade8e
fastsnowx <61731151+fastsnowx@users.noreply.github.com>
2022-11-14 07:46:43 +0000

    [change] classに書き換え

--- models/train.py
+++ models/train.py
@@ -1,48 +1,52 @@
 # %%
 import implicit
-import pymongo
 from pymongo import MongoClient
 import pandas as pd
-import numpy as np
 from scipy.sparse import csr_matrix
-from sklearn.neighbors import NearestNeighbors
-import implicit
-from scipy.sparse import lil_matrix,coo_matrix
+from scipy.sparse import lil_matrix
 from decouple import config
 
-def retrieve_and_preprocess():
-    # data持ってくるところ別のファイルでやりたい
-    MONGODB_API_KEY = config("MONGODB_API_KEY")
-    client = MongoClient(MONGODB_API_KEY)
+class Recommendation():
+    def __init__(self) -> None:
+        
+        def retrieve_and_preprocess():
+            # data持ってくるところ別のファイルでやりたい
+            MONGODB_API_KEY = config("MONGODB_API_KEY")
+            client = MongoClient(MONGODB_API_KEY)
 
-    db = client.reviews
-    collection_Overall = db.ratingOverallState
+            db = client.reviews
+            collection_Overall = db.ratingOverallState
 
-    # 前処理を別でやる？
-    df = pd.DataFrame(list(collection_Overall.find()))
+            # 前処理を別でやる？
+            df = pd.DataFrame(list(collection_Overall.find()))
+            df_pivot = df.pivot_table(index="id",columns="annictId",values="ratingOverallState").fillna(0)
+            annictIdList = list(df_pivot.columns.values)
+            reviewData = df_pivot.values
+            reviewData = csr_matrix(reviewData)
 
-    df_pivot = df.pivot_table(index="id",columns="annictId",values="ratingOverallState").fillna(0)
-    annictIdList = list(df_pivot.columns.values)
-    reviewData = df_pivot.values
-    reviewData = csr_matrix(reviewData)
-    return annictIdList, reviewData
+            return annictIdList, reviewData
 
-def train(data):
-    model = implicit.als.AlternatingLeastSquares(factors=200, iterations=10)
-    model.fit(data, show_progress=True)
-    return model
+        def train(data):
+            model = implicit.als.AlternatingLeastSquares(factors=200, iterations=10)
+            model.fit(data, show_progress=True)
+            return model
 
-def recommend_animes(annictIds: list, models, annictIdList, df_pivot) -> list:
-    user_like = lil_matrix((1, len(df_pivot.columns)))
-    for i in annictIds:
-        if i in annictIdList:
-            user_like[(0, annictIdList.index(i))] = 2.0
-    recommend_index, reccomend_scores = models.recommend(
-    0,
-    user_like.tocsr(),
-    N=10,
-    recalculate_user=True,
-    )
-    reccomend_ids = [annictIdList[i] for i in recommend_index]
-    return reccomend_ids, reccomend_scores
+        self.annictIdList, self.reviewData, self.df_pivot = retrieve_and_preprocess()
+        self.model = train(self.reviewData)
 
+    
+    def recommend_animes(self, annictIds):
+        user_like = lil_matrix((1, len(self.df_pivot.columns)))
+        for i in annictIds:
+            if i in self.annictIdList:
+                user_like[(0, self.annictIdList.index(i))] = 2.0
+
+        recommend_index, reccomend_scores = self.model.recommend(
+            0,
+            user_like.tocsr(),
+            N=10,
+            recalculate_user=True,
+            )
+
+        reccomend_ids = [self.annictIdList[i] for i in recommend_index]
+        return reccomend_ids, reccomend_scores
