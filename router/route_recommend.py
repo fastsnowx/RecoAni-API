6839eb659000393ff2043b721711201bcce8a927
fastsnowx <61731151+fastsnowx@users.noreply.github.com>
2022-11-15 09:47:00 +0000

    [add] 型定義

--- router/route_recommend.py
+++ router/route_recommend.py
@@ -1,11 +1,11 @@
 from typing import List
 from fastapi import APIRouter
 from fastapi import Response, Request, HTTPException, Depends, Query
-from schemas import SuccessMsg
+from schemas import AnimeInfo
 from models.train import Recommendation
 router = APIRouter()
 recommend = Recommendation()
-@router.get("/api/recommend/")
+@router.get("/api/recommend/", response_model=AnimeInfo)
 def get_recommend_annictId(request: Request, user_likes: List[int] = Query(None)):
     recommend_annictId, reccomend_scores = recommend.recommend_animes(user_likes)
     return {
--- schemas.py
+++ schemas.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from decouple import config
 
 class SuccessMsg(BaseModel):
     message: str
+
+class RecommendWorks(BaseModel):
+    annictId: int
+    score: float
+class AnimeInfo(BaseModel):
+    data: list[RecommendWorks]
