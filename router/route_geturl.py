ef7755a26de3b1746eecca5892015166b861604d
re-nan6 <iadhoy2000@gmail.com>
2022-12-01 05:48:52 +0000

    [fix] URLの取得方法を変更

--- router/route_geturl.py
+++ router/route_geturl.py
@@ -6,12 +6,12 @@ from decouple import config
 import requests
 
 router = APIRouter()
+CLIENT_ID = config("CLIENT_ID")
 
 @router.get("/api/geturl/image", response_model=AnimeUrl)
 def get_url_image(request: Request, malAnimeId: str):
-    access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImMzZTQ4MjkyYjk2ZDgxYzUxYTFlMDI1NjYzMjI5ZmZiM2FmNmYxYmNjY2ZkNjQ1OTE0MTFhNzhhMTRjODQzOWFiZWQ5YTNjNGU0N2VmODhlIn0.eyJhdWQiOiI4M2I3NGJkZDBlMzdmMDViZjU5MjJmMWYwODkyMWI3ZCIsImp0aSI6ImMzZTQ4MjkyYjk2ZDgxYzUxYTFlMDI1NjYzMjI5ZmZiM2FmNmYxYmNjY2ZkNjQ1OTE0MTFhNzhhMTRjODQzOWFiZWQ5YTNjNGU0N2VmODhlIiwiaWF0IjoxNjY5NjQ1NTgzLCJuYmYiOjE2Njk2NDU1ODMsImV4cCI6MTY3MjIzNzU4Mywic3ViIjoiMTU5MTEzOTkiLCJzY29wZXMiOltdfQ.ax5VgGTk7Vd4xBEz7iLtXETh5x4kynfAVn49JfRcLWL6foiy1zxKjzp0ShJEhkuxXbL3U7m1o6sR2-5gM43gy9MWf9mCmO3u1M4zR0rn7133IoWIFBWb28x20qdL38l_7JgFhXUjcMHj61asKeVPgWS-TSpKEDubYheJAUrJQIDNn-ctKtX7s10oEa9G6uBhWft7NFX4u8PFg7fBeU8nwGxMJfCATm4Lfj4vZFJb4cT05SyJ53DWsziL25M2ouLiD01gvxwAlZ5Hpcg5y9DR-t533uBKjnnIXwAjyHVR3VG0KmE-tXUfx89fjIpTVqARoW5Lwk-udryBxMlXfe_0_g"
     header = {
-            "Authorization": "Bearer " + access_token,
+            "X-MAL-CLIENT-ID":CLIENT_ID,
             }
     param = {
         "anime_id":malAnimeId,
@@ -29,9 +29,8 @@ def get_url_image(request: Request, malAnimeId: str):
 
 @router.get("/api/geturl/pv", response_model=AnimeUrl)
 def get_url_pv(request: Request, malAnimeId: str):
-    access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImMzZTQ4MjkyYjk2ZDgxYzUxYTFlMDI1NjYzMjI5ZmZiM2FmNmYxYmNjY2ZkNjQ1OTE0MTFhNzhhMTRjODQzOWFiZWQ5YTNjNGU0N2VmODhlIn0.eyJhdWQiOiI4M2I3NGJkZDBlMzdmMDViZjU5MjJmMWYwODkyMWI3ZCIsImp0aSI6ImMzZTQ4MjkyYjk2ZDgxYzUxYTFlMDI1NjYzMjI5ZmZiM2FmNmYxYmNjY2ZkNjQ1OTE0MTFhNzhhMTRjODQzOWFiZWQ5YTNjNGU0N2VmODhlIiwiaWF0IjoxNjY5NjQ1NTgzLCJuYmYiOjE2Njk2NDU1ODMsImV4cCI6MTY3MjIzNzU4Mywic3ViIjoiMTU5MTEzOTkiLCJzY29wZXMiOltdfQ.ax5VgGTk7Vd4xBEz7iLtXETh5x4kynfAVn49JfRcLWL6foiy1zxKjzp0ShJEhkuxXbL3U7m1o6sR2-5gM43gy9MWf9mCmO3u1M4zR0rn7133IoWIFBWb28x20qdL38l_7JgFhXUjcMHj61asKeVPgWS-TSpKEDubYheJAUrJQIDNn-ctKtX7s10oEa9G6uBhWft7NFX4u8PFg7fBeU8nwGxMJfCATm4Lfj4vZFJb4cT05SyJ53DWsziL25M2ouLiD01gvxwAlZ5Hpcg5y9DR-t533uBKjnnIXwAjyHVR3VG0KmE-tXUfx89fjIpTVqARoW5Lwk-udryBxMlXfe_0_g"
     header = {
-            "Authorization": "Bearer " + access_token,
+            "X-MAL-CLIENT-ID":CLIENT_ID,
             }
     param = {
         "anime_id":malAnimeId,
