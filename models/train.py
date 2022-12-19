dc85a013bf27b61ff7e61f6626f39541596a5d15
fastsnowx <61731151+fastsnowx@users.noreply.github.com>
2022-12-19 17:35:42 +0000

    [fix] remove print

--- models/train.py
+++ models/train.py
@@ -11,7 +11,8 @@ client = MongoClient(MONGODB_API_KEY)
 
 db = client.reviews
 
-class Recommendation():
+
+class Recommendation:
     def __init__(self, ratingState: str) -> None:
         def retrieve_and_preprocess(ratingState):
             if ratingState == "ratingOverallState":
@@ -27,31 +28,37 @@ class Recommendation():
             else:
                 raise AssertionError
 
-            print("fetching")
             df = pd.DataFrame(list(collection.find()))
-            print("comp")
-            user_id_categorical = pd.api.types.CategoricalDtype(categories=sorted(df.id.unique()), ordered=True)
-            annictId_categorical = pd.api.types.CategoricalDtype(categories=sorted(df.annictId.unique()), ordered=True)
+            user_id_categorical = pd.api.types.CategoricalDtype(
+                categories=sorted(df.id.unique()), ordered=True
+            )
+            annictId_categorical = pd.api.types.CategoricalDtype(
+                categories=sorted(df.annictId.unique()), ordered=True
+            )
             row = df.id.astype(user_id_categorical).cat.codes
             col = df.annictId.astype(annictId_categorical).cat.codes
             annictIdList = list(sorted(df.annictId.unique()))
             reviewData = csr_matrix(
-                                (df[ratingState],
-                                (row, col)),
-                                shape=(user_id_categorical.categories.size, annictId_categorical.categories.size))
+                (df[ratingState], (row, col)),
+                shape=(
+                    user_id_categorical.categories.size,
+                    annictId_categorical.categories.size,
+                ),
+            )
             columns_size = len(annictIdList)
-            print("pleted")
             return annictIdList, reviewData, columns_size
 
         def train(data):
+            print("training models ...")
             model = implicit.als.AlternatingLeastSquares(factors=200, iterations=10)
             model.fit(data, show_progress=True)
             return model
 
-        self.annictIdList, reviewData, self.columns_size = retrieve_and_preprocess(ratingState)
+        self.annictIdList, reviewData, self.columns_size = retrieve_and_preprocess(
+            ratingState
+        )
         self.model = train(reviewData)
 
-    
     def recommend_animes(self, annictIds):
         user_like = lil_matrix((1, self.columns_size))
         for i in annictIds:
@@ -63,7 +70,7 @@ class Recommendation():
             user_like.tocsr(),
             N=10,
             recalculate_user=True,
-            )
+        )
 
         reccomend_ids = [self.annictIdList[i] for i in recommend_index]
         return reccomend_ids, reccomend_scores
