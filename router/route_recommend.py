09085fdb6c0f5919e707da3788f8d34045b218b5
fastsnowx <61731151+fastsnowx@users.noreply.github.com>
2022-12-19 17:36:37 +0000

    [change] indent

--- router/route_recommend.py
+++ router/route_recommend.py
@@ -3,12 +3,14 @@ from fastapi import APIRouter
 from fastapi import Response, Request, HTTPException, Depends, Query
 from schemas import RecommendAnimeInfo
 from models.train import Recommendation
+
 router = APIRouter()
 recommendOverall = Recommendation(ratingState="ratingOverallState")
 # recommendStory = Recommendation(ratingState="ratingStoryState")
 # recommendMusic = Recommendation(ratingState="ratingMusicState")
 # recommendCharacter = Recommendation(ratingState="ratingCharacterState")
 # recommendAnimation = Recommendation(ratingState="ratingAnimationState")
+
 
 @router.get("/api/recommend/overall", response_model=RecommendAnimeInfo)
 def get_recommend_overall(request: Request, user_likes: List[int] = Query(None)):
@@ -23,6 +25,7 @@ def get_recommend_overall(request: Request, user_likes: List[int] = Query(None))
         ]
     }
 
+
 @router.get("/api/recommend/story", response_model=RecommendAnimeInfo)
 def get_recommend_story(request: Request, user_likes: List[int] = Query(None)):
     recommend_annictId, reccomend_scores = recommendStory.recommend_animes(user_likes)
@@ -35,6 +38,7 @@ def get_recommend_story(request: Request, user_likes: List[int] = Query(None)):
             for singleId, singleScore in zip(recommend_annictId, reccomend_scores)
         ]
     }
+
 
 @router.get("/api/recommend/music", response_model=RecommendAnimeInfo)
 def get_recommend_music(request: Request, user_likes: List[int] = Query(None)):
@@ -49,9 +53,12 @@ def get_recommend_music(request: Request, user_likes: List[int] = Query(None)):
         ]
     }
 
+
 @router.get("/api/recommend/character", response_model=RecommendAnimeInfo)
 def get_recommend_character(request: Request, user_likes: List[int] = Query(None)):
-    recommend_annictId, reccomend_scores = recommendCharacter.recommend_animes(user_likes)
+    recommend_annictId, reccomend_scores = recommendCharacter.recommend_animes(
+        user_likes
+    )
     return {
         "data": [
             {
@@ -62,9 +69,12 @@ def get_recommend_character(request: Request, user_likes: List[int] = Query(None
         ]
     }
 
+
 @router.get("/api/recommend/animation", response_model=RecommendAnimeInfo)
 def get_recommend_animation(request: Request, user_likes: List[int] = Query(None)):
-    recommend_annictId, reccomend_scores = recommendAnimation.recommend_animes(user_likes)
+    recommend_annictId, reccomend_scores = recommendAnimation.recommend_animes(
+        user_likes
+    )
     return {
         "data": [
             {
--- schemas.py
+++ schemas.py
@@ -4,23 +4,31 @@ from decouple import config
 
 CSRF_KEY = config("CSRF_KEY")
 
+
 class CsrfSettings(BaseModel):
     secret_key: str = CSRF_KEY
+
 
 class SuccessMsg(BaseModel):
     message: str
 
+
 class RecommendWorks(BaseModel):
     annictId: int
     score: float
+
+
 class RecommendAnimeInfo(BaseModel):
     data: List[RecommendWorks]
+
 
 class MyAnimeListWorks(BaseModel):
     url: str
 
+
 class MyAnimeListImageInfo(BaseModel):
     data: List[MyAnimeListWorks]
+
 
 class MyAnimeListPVInfo(BaseModel):
     data: List[MyAnimeListWorks]
